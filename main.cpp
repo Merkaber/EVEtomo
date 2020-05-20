a5910af618d518ff4781e7829683c95e49fc8463
Merkaber <social.pio@posteo.jp>
2020-05-20 21:12:43 +0200

    Added screen size retrieval functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
-int main() {
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+int getPrimaryScreenWidth();
+int getPrimaryScreenHeight();
+
+int main()
+{
     std::cout << "Hello, World!" << std::endl;
     return 0;
+}
+
+int getPrimaryScreenWidth()
+{
+#ifdef WIN32
+    return GetSystemMetrics(SM_CXSCREEN);
+#endif
+}
+
+int getPrimaryScreenHeight()
+{
+#ifdef WIN32
+    return GetSystemMetrics(SM_CYSCREEN);
+#endif
 }
